ee8445dd9f9e3584785546da2d6df8d078c86558
Everton Xavier <evertonxavier@hotmail.com>
2023-08-01 17:19:46 -0300

    First commit

---
+++ 05_FisrtStepsInJavawithSpringBoot/rest-with-spring-java/src/main/java/br/com/evertonxavier/Hello.java
@@ -0,0 +1,22 @@
+package br.com.evertonxavier;
+
+public class Hello {
+	
+	private final long id;
+	private final String content;
+	
+	public Hello(long id, String content) {
+		super();
+		this.id = id;
+		this.content = content;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+}
